33586937f8d4cad1583f49dc222aa3151a637a36
Nathan Cox <njcox88@gmail.com>
2018-01-09 15:41:37 -0800

    theme update

--- conf.py
+++ conf.py
@@ -82,7 +82,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = ''
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -100,12 +100,8 @@ html_static_path = ['_static']
 #
 # This is required for the alabaster theme
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
-html_sidebars = {
-    '**': [
-        'relations.html',  # needs 'show_related': True theme option to display
-        'searchbox.html',
-    ]
-}
+html_sidebars = {}
+    
 
 
 # -- Options for HTMLHelp output ------------------------------------------
